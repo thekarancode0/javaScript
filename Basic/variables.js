7f9e5f6b84bd7f83b8d23d01a6251e3101fec7c6
Karan Kumar <161097161+thekarancode0@users.noreply.github.com>
2024-03-25 05:13:09 +0000

    day 1

---
+++ Basic/variables.js
@@ -0,0 +1,22 @@
+//variables are memory location where data stored
+
+const name ="karan"
+let password=1234
+var city="gopalganj"
+
+//constant variable don't change
+// name="kumar"
+//const variables are constant they don't change once declared
+//var are used earlier they are changeable and not a block variable
+//let are block or curly braces variables they also change 
+
+{
+    let change= 123
+}
+// console.log(change) change variable do not access outside the curly ...
+
+{
+    address = "bihar"  //considered as variable declared with var keyword
+}
+// address = "india"
+console.log(address)
