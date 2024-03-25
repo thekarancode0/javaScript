da79fc041323b33fd8646e221e2f1dc0bafdecaa
Karan Kumar <161097161+thekarancode0@users.noreply.github.com>
2024-03-25 05:44:45 +0000

    conversion of Data types

---
+++ Basic/conversion.js
@@ -0,0 +1,35 @@
+let age = "45"
+// console.log(typeof age)
+let ageInnumber = Number(age)
+// console.log(typeof ageInnumber)
+
+
+//string ------->number
+let myname="karan"
+// console.log(typeof myname)
+let mynameinNumber = Number(myname)  //converted but NaN
+// console.log(typeof mynameinNumber) 
+// console.log(mynameinNumber)   
+
+
+// boolean----------->number
+let isPass = true;
+// console.log(typeof isPass)
+ 
+let isPassinNumber = Number(isPass)
+// console.log(typeof isPassinNumber)
+// console.log(isPassinNumber)
+
+//string------->bool
+let course = "0"
+let courseBool= Boolean(course)
+// console.log(typeof courseBool)
+// console.log(courseBool)
+
+//Boolean----->string
+let isfail=false
+console.log(typeof isfail)
+let isfailString= String(isfail)
+console.log(typeof isfailString)
+console.log(isfailString)
+
