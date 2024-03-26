e163d4eb6f2d6a2b636e68cd0038d3937c1be12f
Karan Kumar <161097161+thekarancode0@users.noreply.github.com>
2024-03-26 06:16:09 +0000

    string and its methods

---
+++ Basic/String.js
@@ -0,0 +1,19 @@
+let myname = "karan "
+// console.log("Original String: ", myname)
+// console.log("type of: ", typeof myname)
+// console.log("Upper case: " , myname.toUpperCase())
+
+let city = "Gopalganj, Bihar, India"
+// console.log("Length of String: ",city.length)
+// console.log("Original: ", city)
+// console.log("Value at index : " ,city.charAt(5))
+// console.log("index of char: ", city.indexOf('I'))
+// console.log("replace: ", city.replace(',', '&'))
+// console.log("replace all: ", city.replaceAll(',','**'))
+// console.log("slice: ", city.slice(1,8))
+// console.log("slice: ",city.slice(0))
+// console.log("slice: ",city.slice())
+// console.log("String include: ", city.includes('Bihar'))
+// console.log("String include: ", city.includes('Bih'))
+// console.log("String include: ", city.includes('karan'))
+
