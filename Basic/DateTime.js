3e57d80d3b7ea7c1b6661342a8beda26d1172323
Karan Kumar <161097161+thekarancode0@users.noreply.github.com>
2024-03-26 06:15:52 +0000

    Date and time

--- Basic/DateTime.js
+++ Basic/DateTime.js
@@ -1,4 +1,11 @@
 let mytime = new Date()
 // console.log(mytime.toISOString())
 // console.log(mytime.toLocaleTimeString())
-console.log(mytime.getFullYear())
+// console.log(mytime.getFullYear())
+
+// let newtime = new Date('October 08 , 2002, 23:00:00')
+// console.log(newtime.toLocaleString())
+
+
+// let timenow = Date.now()
+// console.log(timenow)
